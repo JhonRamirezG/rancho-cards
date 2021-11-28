0e7ea313a845245e09285002766cab1ad72f8b14
JhonRmz <jhonramirezgamboa@gmail.com>
2021-11-27 22:20:32 -0600

    Add web server and templates render

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/jhonrmz/rancho-cards/pkg/render"
+)
+
+//* Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html")
+}
+
+//* About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "about.page.html")
+}
+
+//* Shop is the shopping page handler
+func Shop(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "shop.page.html")
+}
+
+//* Offers is the offers page handler
+func Offers(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "offers.page.html")
+}
+
+//* Orders is the order page handler
+func Orders(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "orders.page.html")
+}
